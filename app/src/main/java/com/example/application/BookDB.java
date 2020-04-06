0b2ce84c85ea28fe3efaa474ca8f5ed7bf16786f
popoln3 <popolin3@gmail.com>
2020-04-06 21:02:58 +0800

    initialilation

---
+++ app/src/main/java/com/example/application/BookDB.java
@@ -0,0 +1,14 @@
+package com.example.application;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class BookDB extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+        //啟動stetho
+        Stetho.initializeWithDefaults(this);
+    }
+}
